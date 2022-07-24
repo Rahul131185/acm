3255cae2e94f585db7f0e8180308f8efba01a1d7
Rahul131185 <109912748+Rahul131185@users.noreply.github.com>
2022-07-25 00:43:38 +0530

    Hello

--- acm/src/acm/java/First.java
+++ acm/src/acm/java/First.java
@@ -3,7 +3,7 @@ package acm.java;
 public class First {
 
 	public static void main(String[] args) {  
-		System.out.println("Hello World by Rahul Singh Testing!"); 
+		System.out.println("Hello World by Rahul Singh Hello!"); 
 	}
 
 }
